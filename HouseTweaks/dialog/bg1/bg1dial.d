21cee2e5a35ea18091f45cb684fc73a2d183baa8
ArtemiusI <whuangx10@gmail.com>
2018-05-21 01:34:36 -0400

    v1.2

---
+++ HouseTweaks/dialog/bg1/bg1dial.d
@@ -0,0 +1,56 @@
+I_C_T VAI 6 c0VAI
+== VAI ~If you still wish to turn in bandit scalps, you may speak with Officer Zelas. He will be assigned to Beregost in my stead.~
+DO ~CreateCreature("c0zelas",[685.470],S)
+ActionOverride("c0zelas",MoveToPoint([662.891]))~
+END
+
+EXTEND_TOP BRAGE 4
+++ ~Remain here, Brage. We shall return to Nashkel once I am prepared.~ EXTERN BRAGE brage1
+++ ~Then let's go immediately.~ DO ~DestroyItem("SW2H01")
+GiveItem("SW2H03",LastTalkedToBy)
+GiveItem("POTN12",LastTalkedToBy)
+AddexperienceParty(1000)
+SetGlobal("DMWWRepBoost","GLOBAL",1)
+ClearAllActions()
+StartCutSceneMode()
+StartCutSceneEx("Bragecut",FALSE)~ EXIT
+END
+
+CHAIN BRAGE brage1
+~Tarry not, I beg of you. I can hardly bear the shame of my actions for another moment.~
+DO ~SetGlobal("c0bragewait","GLOBAL",1)~
+EXIT
+
+CHAIN IF WEIGHT #-1
+~Global("c0bragewait","GLOBAL",1)~ THEN BRAGE brage2
+~Are we to return to Nashkel? Please, please! I must face justice for my actions with whatever honor I have left, and I can wait no longer!~
+END
+++ ~Yes, let's go immediately.~ DO ~DestroyItem("SW2H01")
+GiveItem("SW2H03",LastTalkedToBy)
+GiveItem("POTN12",LastTalkedToBy)
+AddexperienceParty(1000)
+SetGlobal("DMWWRepBoost","GLOBAL",1)
+SetGlobal("c0bragewait","GLOBAL",2)
+ClearAllActions()
+StartCutSceneMode()
+StartCutSceneEx("Bragecut",FALSE)~ EXIT
+++ ~Not yet.~ EXIT
+
+BEGIN C0ZELAS
+
+IF ~NumTimesTalkedTo(0)~ THEN BEGIN a1
+SAY ~Greetings, <CHARNAME>. Officer Vai has spoken highly of your character, and I am to take over her duties here in Beregost. If you wish to exchange any more bandit scalps in the future, speak to me.~
++ ~PartyHasItem("MISC86")~ + ~I have bandit scalps to turn in.~ DO ~StartStore("STOVAI2",LastTalkedToBy(Myself))~ EXIT
+++ ~Farewell.~ EXIT
+END
+
+IF ~!NumTimesTalkedTo(0)~ THEN BEGIN a2
+SAY ~Good <DAYNIGHT> to you, <CHARNAME>. Do you need anything?~
++ ~PartyHasItem("MISC86")~ + ~I have bandit scalps to turn in.~ DO ~StartStore("STOVAI2",LastTalkedToBy(Myself))~ EXIT
+++ ~Not at the moment.~ EXIT
+END
+
+IF WEIGHT #-1 ~ReputationLT(LastTalkedToBy,5)~ THEN BEGIN a3
+SAY ~You have some nerve showing your face around here. Your reputation precedes you, <CHARNAME>. In the name of the Flaming Fist, you are sentenced to death!~
+IF ~~ DO ~Enemy()~ EXIT
+END
---
+++ HouseTweaks/dialog/bg2/bg2dial.d
@@ -0,0 +1,64 @@
+REPLACE_ACTION_TEXT elgea
+~SetGlobal("ElgeaFree","GLOBAL",1)~
+~SetGlobal("ElgeaFree","GLOBAL",1)
+SetGlobalTimer("C0ElgeaRewardTimer","GLOBAL",THREE_DAYS)~
+
+CHAIN IF WEIGHT #-1 ~Global("C0ElgeaReward","GLOBAL",1)~ THEN WELTHER a1
+~Greetings, friend. Might I have a moment of your time?~
+END
+++ ~What is it?~ + a1.1
+++ ~Make it quick.~ + a1.1
+
+CHAIN WELTHER a1.1
+~I bring regards from Lady Elgea, whom you rescued from those wretched kidnappers days ago. She has asked me to personally deliver these to you.~
+= ~It was originally agreed for us to exchange it as ransom for the lady's freedom, but since that is no longer necessary, and as you are still owed a reward for your actions, she feels it is appropriate that you take it instead.~
+END
+++ ~It will do. Thank you.~ + a1.2
+++ ~I'll find a use for these... somehow.~ + a1.2
+
+CHAIN WELTHER a1.2
+~Farewell. Good fortune to you.~
+DO ~GiveItem("MISC5O",Player1)
+EscapeArea()~ EXIT
+
+BEGIN C0DMALTR
+
+CHAIN IF WEIGHT #-1 ~True()~ THEN C0DMALTR a1
+~An inscription on the demonic statue reads: 
+Place an animal sacrifice before great Demogorgon, and awaken his children from their long sleep.~
+END
++ ~!Alignment(Lasttalkedtoby(Myself),MASK_EVIL)
+OR(2)
+Class(Lasttalkedtoby(Myself),PALADIN)
+Class(Lasttalkedtoby(Myself),CLERIC_ALL)~ + ~Utter a sanctifying prayer upon the altar.~ + a1.1
+++ ~Strike the altar.~ DO ~ActionOverride(Player1,ApplyDamage(Myself,50,MAGIC))
+ActionOverride(Player1,ApplySpell(Myself,WIZARD_ENERGY_DRAIN))
+ActionOverride(Player2,ApplyDamage(Myself,50,MAGIC))
+ActionOverride(Player2,ApplySpell(Myself,WIZARD_ENERGY_DRAIN))
+ActionOverride(Player3,ApplyDamage(Myself,50,MAGIC))
+ActionOverride(Player3,ApplySpell(Myself,WIZARD_ENERGY_DRAIN))
+ActionOverride(Player4,ApplyDamage(Myself,50,MAGIC))
+ActionOverride(Player4,ApplySpell(Myself,WIZARD_ENERGY_DRAIN))
+ActionOverride(Player5,ApplyDamage(Myself,50,MAGIC))
+ActionOverride(Player5,ApplySpell(Myself,WIZARD_ENERGY_DRAIN))
+ActionOverride(Player6,ApplyDamage(Myself,50,MAGIC))
+ActionOverride(Player6,ApplySpell(Myself,WIZARD_ENERGY_DRAIN))~ + a1.1
+++ ~Leave.~ EXIT
+
+CHAIN C0DMALTR a1.1
+~You sense the rage of several demonic presences in response to your desecration of the unholy altar...~
+DO ~StartCutsceneMode()
+	ReallyForceSpell(LastTrigger,RED_EFFECT)
+    CreateVisualEffect("SPPORTAL",[2724.1143])
+    CreateVisualEffect("SPPORTAL",[3280.1348])
+    CreateVisualEffect("SPPORTAL",[2929.1562])
+    CreateVisualEffect("SPPORTAL",[3071.1172])
+    CreateVisualEffect("SPPORTAL",[3094.1695])
+    Wait(3)
+    CreateCreature("uddeath",[2929.1562],E)
+    CreateCreature("uddeath",[2724.1143],E)
+    CreateCreature("uddeath",[3280.1348],E)
+    CreateCreature("uddeath2",[3071.1172],E)
+    CreateCreature("uddeath",[3094.1695],E)
+	SetGlobal("spawnKnights","AR2402",1)
+	EndCutsceneMode()~ EXIT
